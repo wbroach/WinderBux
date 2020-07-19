b8a8a9a7ec1ec299fc28b5f7a1ae3878b38fca30
Will <william.broach@gmail.com>
2020-07-19 16:07:47 -0400

    added Json dependency & actual blockchain

--- src/main/java/winderbux/WinderBux.java
+++ src/main/java/winderbux/WinderBux.java
@@ -1,16 +1,18 @@
 package winderbux;
 
+import java.util.ArrayList; 
+import com.google.gson.GsonBuilder;
+
 public class WinderBux {
 
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+
     public static void main(String[] args) {
-	Block genesisBlock = new Block("Hi im the first block", "0");
-	System.out.println("Hash for block 1 : " + genesisBlock.hash);
-		
-	Block secondBlock = new Block("Yo im the second block",genesisBlock.hash);
-	System.out.println("Hash for block 2 : " + secondBlock.hash);
+	blockchain.add(new Block("Genesis block", "0"));		
+	blockchain.add(new Block("Second block",blockchain.get(blockchain.size()-1).hash)); 
+	blockchain.add(new Block("Third block",blockchain.get(blockchain.size()-1).hash));
 	
-	Block thirdBlock = new Block("Hey im the third block",secondBlock.hash);
-	System.out.println("Hash for block 3 : " + thirdBlock.hash);
+	System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(blockchain));
     }
 
 }
